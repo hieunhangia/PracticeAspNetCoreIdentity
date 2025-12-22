563e7b89b51b3915e386eee36964e039787b9bb7
hieunhangia <hieuvippro531@gmail.com>
2025-12-22 16:52:47 +0700

    update use guid as user primary key

--- PracticeAspNetCoreIdentity.Server/AppDbContext.cs
+++ PracticeAspNetCoreIdentity.Server/AppDbContext.cs
@@ -1,7 +1,8 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using PracticeAspNetCoreIdentity.Server.Models;
 
 namespace PracticeAspNetCoreIdentity.Server;
 
-public class AppDbContext(DbContextOptions<AppDbContext> options) : IdentityDbContext<CustomUser>(options);
+public class AppDbContext(DbContextOptions<AppDbContext> options) : IdentityDbContext<CustomUser, IdentityRole<Guid>, Guid>(options);
--- PracticeAspNetCoreIdentity.Server/Models/CustomUser.cs
+++ PracticeAspNetCoreIdentity.Server/Models/CustomUser.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Identity;
 
 namespace PracticeAspNetCoreIdentity.Server.Models;
 
-public class CustomUser : IdentityUser
+public class CustomUser : IdentityUser<Guid>
 {
     public string? FullName { get; set; }
 }
