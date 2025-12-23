3127497c8f170805e560bfceeb0cea97332ae156
hieunhangia <hieuvippro531@gmail.com>
2025-12-23 15:09:53 +0700

    use top-level code for Program.cs

--- PracticeAspNetCoreIdentity.Client/Program.cs
+++ PracticeAspNetCoreIdentity.Client/Program.cs
@@ -1,26 +1,19 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using PracticeAspNetCoreIdentity.Client;
 
-namespace PracticeAspNetCoreIdentity.Client;
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
-public abstract class Program
+builder.Services.AddScoped<CookieHandler>();
+builder.Services.AddScoped(sp =>
 {
-    public static async Task Main(string[] args)
-    {
-        var builder = WebAssemblyHostBuilder.CreateDefault(args);
-        builder.RootComponents.Add<App>("#app");
-        builder.RootComponents.Add<HeadOutlet>("head::after");
+    var cookieHandler = sp.GetRequiredService<CookieHandler>();
+    cookieHandler.InnerHandler = new HttpClientHandler();
+    return new HttpClient(cookieHandler) {
+        BaseAddress = new Uri(builder.Configuration["WebApiBaseUrl"]!)
+    };
+});
 
-        builder.Services.AddScoped<CookieHandler>();
-        builder.Services.AddScoped(sp =>
-        {
-            var cookieHandler = sp.GetRequiredService<CookieHandler>();
-            cookieHandler.InnerHandler = new HttpClientHandler();
-            return new HttpClient(cookieHandler) {
-                BaseAddress = new Uri(builder.Configuration["WebApiBaseUrl"]!)
-            };
-        });
-
-        await builder.Build().RunAsync();
-    }
-}
+await builder.Build().RunAsync();
