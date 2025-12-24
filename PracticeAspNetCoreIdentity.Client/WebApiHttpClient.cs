50630cf28534dd2ff4bc9e2821260a762bbcd0f1
hieunhangia <hieuvippro531@gmail.com>
2025-12-24 20:47:00 +0700

    add logout feature in Home.razor

--- PracticeAspNetCoreIdentity.Client/WebApiHttpClient.cs
+++ PracticeAspNetCoreIdentity.Client/WebApiHttpClient.cs
@@ -7,14 +7,17 @@ public class WebApiHttpClient(HttpClient client)
 {
     public async Task<HttpResponseMessage> LoginAsync(string email, string password, bool useCookie = true)
         => await client.PostAsJsonAsync(useCookie ? "login?useCookies=true" : "login", new { email, password });
-    
+
     public async Task<HttpResponseMessage> RegisterAsync(string email, string password)
         => await client.PostAsJsonAsync("register", new { email, password });
-    
-    public async  Task<UserInfo?> GetUserInfoAsync()
+
+    public async Task<UserInfo?> GetUserInfoAsync()
     {
         using var userResponse = await client.GetAsync("manage/info");
         if (userResponse.IsSuccessStatusCode) return await userResponse.Content.ReadFromJsonAsync<UserInfo>();
         return null;
     }
+
+    public async Task<HttpResponseMessage> CookieLogoutAsync()
+        => await client.PostAsync("logout", null);
 }
