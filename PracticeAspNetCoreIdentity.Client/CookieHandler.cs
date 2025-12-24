eea29d49b74df9d1ce6b5dee231d3a0e1d1c1399
hieunhangia <hieuvippro531@gmail.com>
2025-12-24 14:24:48 +0700

    remove unnecessary async/await in SendAsync method in CookieHandler

--- PracticeAspNetCoreIdentity.Client/CookieHandler.cs
+++ PracticeAspNetCoreIdentity.Client/CookieHandler.cs
@@ -4,9 +4,9 @@ namespace PracticeAspNetCoreIdentity.Client;
 
 public class CookieHandler : DelegatingHandler
 {
-    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+    protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
     {
         request.SetBrowserRequestCredentials(BrowserRequestCredentials.Include);
-        return await base.SendAsync(request, cancellationToken);
+        return base.SendAsync(request, cancellationToken);
     }
 }
