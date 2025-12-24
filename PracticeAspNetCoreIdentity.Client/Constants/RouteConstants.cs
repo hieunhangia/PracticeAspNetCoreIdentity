bda2864da7517f5da376624b2016ddbba00c9082
hieunhangia <hieuvippro531@gmail.com>
2025-12-24 23:30:47 +0700

    refactor routing in Razor components to use RouteConstants for improved maintainability

---
+++ PracticeAspNetCoreIdentity.Client/Constants/RouteConstants.cs
@@ -0,0 +1,9 @@
+namespace PracticeAspNetCoreIdentity.Client.Constants;
+
+public static class RouteConstants
+{
+    public const string NotFound = "/not-found";
+    public const string Home = "/";
+    public const string Login = "/login";
+    public const string Register = "/register";
+}
