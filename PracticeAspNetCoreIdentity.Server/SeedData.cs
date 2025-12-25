7b71a565d728938bfc2e000df7b38adb31bbb269
hieunhangia <hieuvippro531@gmail.com>
2025-12-25 13:56:18 +0700

    update database initialization to optionally drop existing database

--- PracticeAspNetCoreIdentity.Server/SeedData.cs
+++ PracticeAspNetCoreIdentity.Server/SeedData.cs
@@ -8,14 +8,18 @@ public static class SeedData
 {
     private static readonly string[] roles = [UserRole.Administrator, UserRole.Manager, UserRole.User];
 
-    public static async Task InitializeAsync(IServiceProvider serviceProvider, IConfiguration configuration)
+    public static async Task InitializeAsync(IServiceProvider serviceProvider, IConfiguration configuration,
+        bool dropExistDatabase = false)
     {
         await using var dbContext = serviceProvider.GetRequiredService<AppDbContext>();
+
+        if (dropExistDatabase) await dbContext.Database.EnsureDeletedAsync();
+
         await dbContext.Database.EnsureCreatedAsync();
-        
+
         using var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole<Guid>>>();
         using var userManager = serviceProvider.GetRequiredService<UserManager<CustomUser>>();
-        
+
         foreach (var role in roles)
         {
             if (!await roleManager.RoleExistsAsync(role)) await roleManager.CreateAsync(new IdentityRole<Guid>(role));
@@ -26,17 +30,11 @@ public static class SeedData
         {
             var adminUser = new CustomUser
             {
-                UserName = adminEmail, 
+                UserName = adminEmail,
                 Email = adminEmail
             };
             var result = await userManager.CreateAsync(adminUser, configuration["AdminPassword"]!);
             if (result.Succeeded) await userManager.AddToRolesAsync(adminUser, roles);
         }
-    }
-    
-    public static async Task EnsureDeletedAsync(IServiceProvider serviceProvider)
-    {
-        await using var dbContext = serviceProvider.GetRequiredService<AppDbContext>();
-        await dbContext.Database.EnsureDeletedAsync();
     }
 }
