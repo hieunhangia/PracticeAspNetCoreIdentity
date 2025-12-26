41bd369ffc0a95d59280788ea554fe061684f750
hieunhangia <hieuvippro531@gmail.com>
2025-12-26 22:34:11 +0700

    add account management views and API methods for locking and unlocking accounts in front end

--- PracticeAspNetCoreIdentity.Client/Constants/Route.cs
+++ PracticeAspNetCoreIdentity.Client/Constants/Route.cs
@@ -6,7 +6,10 @@ public static class Route
     public const string Home = "/";
     public const string Login = "/login";
     public const string Register = "/register";
-    
+
+    public const string AdminAllAccounts = "/admin/all-accounts";
+    public const string AdminAccountDetail = "/admin/account-detail";
+
     public const string AddNote = "/add-note";
     public const string UpdateNote = "/update-note";
     public const string AllNotes = "/all-notes";
--- PracticeAspNetCoreIdentity.Client/WebApiHttpClient.cs
+++ PracticeAspNetCoreIdentity.Client/WebApiHttpClient.cs
@@ -1,10 +1,12 @@
 using System.Net.Http.Json;
+using PracticeAspNetCoreIdentity.Shared.Constants;
 using PracticeAspNetCoreIdentity.Shared.Models;
 
 namespace PracticeAspNetCoreIdentity.Client;
 
 public class WebApiHttpClient(HttpClient client)
 {
+    // Authentication & User Management
     public async Task<HttpResponseMessage> CookieLoginAsync(string email, string password)
         => await client.PostAsJsonAsync("login?useCookies=true", new { email, password });
 
@@ -19,16 +21,36 @@ public class WebApiHttpClient(HttpClient client)
 
     public async Task<HttpResponseMessage> GetUserRolesAsync()
         => await client.GetAsync("manage/roles");
-    
+
+
+    // Account Management
+    public async Task<HttpResponseMessage> GetAllAccountsAsync(int page = 1, int pageSize = 10,
+        string orderBy = AccountOrderBy.EmailAsc)
+        => await client.GetAsync($"accounts?page={page}&pageSize={pageSize}&orderBy={orderBy}");
+
+    public async Task<HttpResponseMessage> GetAccountCountAsync()
+        => await client.GetAsync("accounts/count");
+
+    public async Task<HttpResponseMessage> GetAccountByIdAsync(Guid id)
+        => await client.GetAsync($"accounts/{id}");
+
+    public async Task<HttpResponseMessage> LockOutAccountAsync(Guid id, long lockoutTimeInSecond)
+        => await client.PostAsJsonAsync($"accounts/{id}/lockout", lockoutTimeInSecond);
+
+    public async Task<HttpResponseMessage> UnlockAccountAsync(Guid id)
+        => await client.PostAsync($"accounts/{id}/unlock", null);
+
+
+    // User Notes Management
     public async Task<HttpResponseMessage> GetAllNotesAsync()
         => await client.GetAsync("notes");
-    
+
     public async Task<HttpResponseMessage> GetNoteByIdAsync(Guid id)
         => await client.GetAsync($"notes/{id}");
-    
+
     public async Task<HttpResponseMessage> AddNoteAsync(CreateUpdateUserNoteRequest request)
         => await client.PostAsJsonAsync("notes", request);
-    
+
     public async Task<HttpResponseMessage> UpdateNoteAsync(Guid id, CreateUpdateUserNoteRequest request)
         => await client.PutAsJsonAsync($"notes/{id}", request);
 
