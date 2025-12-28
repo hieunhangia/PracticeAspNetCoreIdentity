1e77ff9c246e06f99e68c0ff2aff4c674a9bd8ad
hieunhangia <hieuvippro531@gmail.com>
2025-12-28 22:48:07 +0700

    update email confirmation token expired time

--- PracticeAspNetCoreIdentity.Server/Identity/Constants/TokenExpiredTime.cs
+++ PracticeAspNetCoreIdentity.Server/Identity/Constants/TokenExpiredTime.cs
@@ -2,6 +2,6 @@ namespace PracticeAspNetCoreIdentity.Server.Identity.Constants;
 
 public static class TokenExpiredTime
 {
-    public const int EmailConfirmationHours = 1;
+    public const int EmailConfirmationHours = 2;
     public const int PasswordResetCodeMinutes = 1;
 }
