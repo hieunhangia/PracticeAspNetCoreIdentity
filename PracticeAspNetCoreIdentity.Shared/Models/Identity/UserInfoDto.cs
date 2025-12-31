fd25ffa6700d069e2868e91a2208239089b24853
hieunhangia <hieuvippro531@gmail.com>
2025-12-31 14:57:24 +0700

    make Email property nullable in UserInfoDto

--- PracticeAspNetCoreIdentity.Shared/Models/Identity/UserInfoDto.cs
+++ PracticeAspNetCoreIdentity.Shared/Models/Identity/UserInfoDto.cs
@@ -2,6 +2,6 @@ namespace PracticeAspNetCoreIdentity.Shared.Models.Identity;
 
 public class UserInfoDto
 {
-    public string Email { get; set; } = string.Empty;
+    public string? Email { get; set; }
     public bool IsEmailConfirmed { get; set; }
 }
